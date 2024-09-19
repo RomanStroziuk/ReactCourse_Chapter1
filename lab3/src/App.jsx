c86122b747cf98d30c8388a87efd5ed02a5e7de5
roma1st <roman.stroziuk@oa.edu.ua>
2024-09-19 14:56:46 +0200

    lab3

---
+++ lab3/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './App.css';
+import AddToDOComponent from './components/AddToDoComponent';
+import ToDoTable from './components/ToDoTable';
+import SearchInput from './components/SearchInput';
+
+function App() {
+  const [toDos, setToDos] = useState([]);
+  const [newToDo, setNewToDo] = useState({ title: '' });
+  const [searchTerm, setSearchTerm] = useState('');
+
+  function handleNewTitleChange(event) {
+    setNewToDo({ id: new Date().getTime(), title: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (newToDo.title) {
+      setToDos([...toDos, newToDo]);
+      setNewToDo({ title: '' }); // Clear the input after adding
+    }
+  }
+
+  function handleDelete(id) {
+    setToDos(toDos.filter(toDo => toDo.id !== id));
+  }
+
+  function handleSearchChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  const filteredToDos = toDos.filter(toDo =>
+    toDo.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <>
+      <SearchInput searchValue={searchTerm} onSearchChange={handleSearchChange} />
+      <AddToDOComponent
+        title={newToDo.title}
+        onTitleChange={handleNewTitleChange}
+        onSubmit={handleSubmit}
+      />
+      <ToDoTable toDos={filteredToDos} onDelete={handleDelete} />
+    </>
+  );
+}
+
+export default App;
---
+++ lab3/src/components/ToDoTable.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./ToDoTable.css"; // Імпортуємо файл з CSS
+
+const ToDoTable = ({ toDos, onDelete }) => {
+  return (
+    <table className="todo-table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {toDos.map((toDo) => (
+          <tr key={toDo.id.toString()}>
+            <td>{toDo.id.toString()}</td>
+            <td>{toDo.title}</td>
+            <td>
+              <button className="delete-button" onClick={() => onDelete(toDo.id)}>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ToDoTable;
